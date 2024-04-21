e4a62fb60ec36c8bc58f1ee7ec39af93b28480bc
Yinka ☯️ <johnadex60@gmail.com>
2024-04-21 13:20:40 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,2 +1,16 @@
-#!/usr/bin/env bash
-# 
+#configures an ssh config file to contain some content
+include stdlib
+
+file_line {'Turning off passwd authentication':
+    ensure  => present,
+    path    => '/etc/ssh/ssh_config',
+    line    => '	PasswordAuthentication no',
+    replace => true
+}
+
+file_line {'constant Identity file':
+    ensure  => present,
+    path    => '/etc/ssh/ssh_config',
+    line    => '	IdentityFile ~/.ssh/school',
+    replace => true
+}
