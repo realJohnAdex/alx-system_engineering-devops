0894c5701a3a06102e8fb1f802b41c56e6fb8a37
Yinka ☯️ <johnadex60@gmail.com>
2024-05-27 12:33:24 +0000

    chore: Update Puppet manifest to install Python 3.8.10, Flask 2.1.0, and Werkzeug 2.2.1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,13 @@ package { 'flask':
     ensure   => '2.1.0',
     provider => 'pip3',
 }
+
+package { 'python':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.2.1',
+  provider => 'pip3',
+}
